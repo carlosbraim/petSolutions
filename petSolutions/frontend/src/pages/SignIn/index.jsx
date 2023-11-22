4b7bfad564064c73f5b884989c0470e37fec5593
carlos.franca <carlosbraim2010@gmail.com>
2023-11-22 16:29:57 -0300

    Login com logo

--- petSolutions/frontend/src/pages/SignIn/index.jsx
+++ petSolutions/frontend/src/pages/SignIn/index.jsx
@@ -42,66 +42,69 @@ export function SignIn(){
                 <small>{user.email}</small>
             </div>
             
-            <div className="login">
-                <div className="login-logo">
-                    <img src="https://cdn.icon-icons.com/icons2/3862/PNG/512/login_icon_240424.png"
-                    alt="Login App"></img>
-                </div>
-
-                <div className="login-camp">
-                    <h1>LOGIN</h1>
-                    <div className="login-input-email">
-                        <Envelope></Envelope>
-                        <input
-                            type="email"
-                            placeholder="Digite seu e-mail"   
-                            value={email}      
-                            onChange={e => setEmail(e.target.value)}                   
-                        ></input>
+            <div className="primary-field">
+                <div className="login">
+                    <div className="login-logo">
+                    <h1>Pet Solution</h1> 
+                    <img src="https://gifs.eco.br/wp-content/uploads/2023/05/imagens-de-patinha-de-cachorro-png-2.png"
+                        alt="Login App"></img>
                     </div>
 
-                    <div className="login-input-password">
-                        <Lock></Lock>
-                        <input                                
-                                placeholder="Digite sua senha"    
-                                type={show ? "text" : "password"}
-                                value={password}      
-                                onChange={e => setPassword(e.target.value)}                         
-                        ></input>     
-                        <div className="login-eye">
-                            {show ? (
-                                <Eye
-                                    size={20}
-                                    onClick={handleClick}
-                                ></Eye>
-                            ) : (
-                                <EyeSlash
-                                    size={20}
-                                    onClick={handleClick}
-                                ></EyeSlash>
-                            )}
-                        </div>                   
-                    </div>
-                    <button type="submit">
-                         Entrar
-                    </button>
-                    
-                    <div className="login-inscricao">
-                        <h4>Não tem uma conta?</h4>
-                        <a href="url_da_sua_pagina_de_inscricao" class="link-inscricao">
-                            Inscrever-se
-                        </a>
-                    </div>
+                    <div className="login-camp">
+                        <h1>LOGIN</h1>  
+                        <div className="login-input-email">
+                            <Envelope></Envelope>
+                            <input
+                                type="email"
+                                placeholder="Digite seu e-mail"   
+                                value={email}      
+                                onChange={e => setEmail(e.target.value)}                   
+                            ></input>
+                        </div>
 
-                    <div className="login-google">
-                        <h4>Logar Com</h4>
-                        <button type="button" className="button" onClick={handleGoogleSignIn}>
-                            <FcGoogle />                        
-                        </button> 
+                        <div className="login-input-password">
+                            <Lock></Lock>
+                            <input                                
+                                    placeholder="Digite sua senha"    
+                                    type={show ? "text" : "password"}
+                                    value={password}      
+                                    onChange={e => setPassword(e.target.value)}                         
+                            ></input>     
+                            <div className="login-eye">
+                                {show ? (
+                                    <Eye
+                                        size={20}
+                                        onClick={handleClick}
+                                    ></Eye>
+                                ) : (
+                                    <EyeSlash
+                                        size={20}
+                                        onClick={handleClick}
+                                    ></EyeSlash>
+                                )}
+                            </div>                   
+                        </div>
+                        <button type="submit">
+                            Entrar
+                        </button>
+                        
+                        <div className="login-inscricao">
+                            <h4>Não tem uma conta?</h4>
+                            <a href="url_da_sua_pagina_de_inscricao" class="link-inscricao">
+                                Inscrever-se
+                            </a>
+                        </div>
+
+                        <div className="login-google">
+                            <h4>Logar Com</h4>
+                            <button type="button" className="button" onClick={handleGoogleSignIn}>
+                                <FcGoogle />                        
+                            </button> 
+                        </div>
+                        
                     </div>
-                    
-                </div>
-            </div>  
+                </div>  
+            </div>
 
 
               
